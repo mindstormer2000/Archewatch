431157c9ebad82c67b0d7d1eb3af181705569c61
Caiden Kehrer <abbotjunk@gmail.com>
2018-05-10 21:17:57 -0600

    First commit

---
+++ Assets/Scripts/PcController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+[RequireComponent(typeof(NavMeshAgent))]
+public class PcController : MonoBehaviour {
+    public NavMeshAgent agent;
+
+	void Start()
+	{
+		
+	}
+	void Update()
+	{
+        if (Input.GetKey("Forward")) {
+            agent.destination = this.gameObject.transform.forward*3;
+
+        }
+	}
+}
